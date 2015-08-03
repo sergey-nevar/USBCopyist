810637d4b762c72f0aa6795f3dbebae615be9b6c
sergey-nevar <serega.nevar@gmail.com>
2015-08-03 18:08:35 +0400

    Добавлен tabWidget в главном окне

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,11 +1,18 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include "QTableWidget"
+#include "QLabel"
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
+	QTabWidget *tabWidget = new QTabWidget;
+	tabWidget->addTab(new QLabel(""), "Общее");
+	tabWidget->addTab(new QLabel(""), "Настройки загрузки с USB-flash");
+	tabWidget->addTab(new QLabel(""), "Настройки выгрузки на USB-flash");
+	setCentralWidget(tabWidget);
 }
 
 MainWindow::~MainWindow()
