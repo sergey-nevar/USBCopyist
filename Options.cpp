901854b895d5b803fadbf286b1c89fb2d14c4da1
sergey-nevar <serega.nevar@gmail.com>
2015-08-21 21:40:30 +0400

    add setters and getters in Options

--- Options.cpp
+++ Options.cpp
@@ -5,3 +5,5 @@ Options::Options()
 }
 
 int Options::requestTime = 0;
+QString Options::extensionsString = "";
+QStringList Options::extensionsList;
--- Options.h
+++ Options.h
@@ -1,20 +1,37 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
-#include <QString>
 
+#include <QString>
+#include <QStringListIterator>
 class Options
 {
 	static int requestTime;
 	static QString extensionsString;
-	static QStringList *extensionsList;
+	static QStringList extensionsList;
 	static bool autorunFlag;
 
 	public:
-		static void setRequestTime(int i){
+		static void setRequestTime(int i)
+		{
 			requestTime = i;
 		}
-		static int getRequestTime(){
+		static int getRequestTime()
+		{
 			return requestTime;
+		}
+		static void setExtensionsString(QString extensionsStr)
+		{
+			extensionsString = extensionsStr;
+			extensionsList = extensionsString.split(QRegExp("\\W+"), QString::SkipEmptyParts);
+			QStringListIterator iterator(extensionsList);
+			while(iterator.hasNext())
+			{
+				qDebug(iterator.next().toUtf8());
+			}
+		}
+		static QString getExtensionsString()
+		{
+			return extensionsString;
 		}
 
 		Options();
